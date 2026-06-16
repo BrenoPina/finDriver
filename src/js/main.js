469c6df93eb93c96c67ed74817fd550e005fd67c
Breno Pina <breno.rodrigues08@hotmail.com>
2026-06-16 10:21:09 -0300

    feat: add dynamic search field for drivers and customers

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import ridesMock from '../../data.js';
 import currencyFormatter from './utils.js';
 
+const searchInput = document.querySelector('#search__input');
+const driverContainer = document.querySelector('.driver-card-template');
+
 const statusConfig = {
   em_andamento: 'Em andamento',
   pendente: 'Pendente',
@@ -22,11 +25,11 @@ function renderRideStats(rides) {
     { total: 0, finalizada: 0, em_andamento: 0, pendente: 0, cancelada: 0 }
   );
 
-  document.querySelector('.sidebar__label-all').textContent = stats.total;
-  document.querySelector('.sidebar__label-finalizada').textContent = stats.finalizada;
-  document.querySelector('.sidebar__label-em_andamento').textContent = stats.em_andamento;
-  document.querySelector('.sidebar__label-pendente').textContent = stats.pendente;
-  document.querySelector('.sidebar__label-cancelada').textContent = stats.cancelada;
+  document.querySelector('#sidebar__label-all').textContent = stats.total;
+  document.querySelector('#sidebar__label-finalizada').textContent = stats.finalizada;
+  document.querySelector('#sidebar__label-em_andamento').textContent = stats.em_andamento;
+  document.querySelector('#sidebar__label-pendente').textContent = stats.pendente;
+  document.querySelector('#sidebar__label-cancelada').textContent = stats.cancelada;
 }
 
 function renderDrivers(drivers) {
@@ -38,7 +41,6 @@ function renderDrivers(drivers) {
 
   drivers.forEach(driver => {
     const cardClone = template.content.cloneNode(true);
-
     cardClone.querySelector('.driver__id').textContent = driver.id;
     cardClone.querySelector('.driver__status').textContent = statusConfig[driver.status];
     cardClone.querySelector('.driver__status').classList.add(`driver__status--${driver.status}`);
@@ -47,12 +49,30 @@ function renderDrivers(drivers) {
     cardClone.querySelector('.driver__client-name').textContent = driver.client;
     cardClone.querySelector('.driver__origin-text').textContent = driver.origin;
     cardClone.querySelector('.driver__destiny-text').textContent = driver.destiny;
-
     fragment.appendChild(cardClone);
   });
-
   driversContainer.appendChild(fragment);
 }
+
+function filterRides(term) {
+  const formattedTerm = term.toLowerCase().trim();
+
+  if (!formattedTerm) return ridesMock;
+
+  return ridesMock.filter(ride => {
+    const matchDriver = ride.driver.toLocaleLowerCase().includes(formattedTerm);
+
+    const matchClient = ride.client.toLowerCase().includes(formattedTerm);
+
+    return matchDriver || matchClient;
+  });
+}
+
+searchInput.addEventListener('input', event => {
+  const valorDigitado = event.target.value;
+  const dadosFiltrados = filterRides(valorDigitado);
+  renderDrivers(dadosFiltrados);
+});
 
 renderRideStats(ridesMock);
 renderDrivers(ridesMock);
